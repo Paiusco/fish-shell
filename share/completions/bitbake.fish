df6780fce7832a7c9813850354c268f51927e765
Paiusco <>
2023-04-23 10:54:27 +0200

    First commit

---
+++ share/completions/bitbake.fish
@@ -0,0 +1,6 @@
+# fish completion for bitbake
+
+
+complete -c bitbake -l version -d "Show Bitbake's version number and exit"
+complete -c bitbake -s h -l help -d "Show this help message and exit"
+complete -c bitbake -s k -l continue -d "Continue as much as possible after an error.
